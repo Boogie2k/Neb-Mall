ecc8bcd1038dba76115186d71f5adfff16e1846d
Boogie2k <oghosabenjamin@gmail.com>
2023-09-11 20:58:47 +0100

    edited the about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,18 +11,7 @@ export default function ShippingPage({mainMenu, footerMenu}: IPageProps) {
 				<h1 className='page-heading page-heading_h1  page-heading_m-h1'>About</h1>
 				<div className='text-container'>
 					<p>
-						Lorem ipsum dolor sit amet consectetur adipiscing elit semper penatibus purus, neque sodales fames
-						pellentesque luctus fringilla libero dis ornare cursus sociosqu, elementum commodo efficitur mauris dictum
-						tortor per ultricies a. Venenatis turpis eleifend proin a dapibus ullamcorper conubia odio vel, porta nulla
-						gravida habitasse facilisi quisque class non aliquam velit, donec nostra amet ante montes ad varius mauris.
-						Maecenas urna laoreet sociosqu tempus rutrum quis ad diam, habitant morbi augue ornare orci a suscipit faucibus,
-						felis nostra senectus at mollis et gravida. Tempus nulla nec etiam mollis, duis lacinia dictum netus,
-						dignissim mi facilisis. Euismod gravida mollis integer class convallis lacinia ad, elit id pharetra ac
-						bibendum iaculis augue lorem, purus at scelerisque posuere dictumst dis. Risus eleifend maecenas cras ac
-						conubia proin nisl, ultrices mollis sem et molestie dolor facilisis, venenatis neque eget maximus quis per.
-						Ante vehicula interdum pulvinar sodales faucibus, metus dignissim orci habitasse. Id dui in amet vehicula luctus
-						lobortis facilisi gravida dis litora diam sapien eget, tortor semper erat turpis pretium odio tempor porta
-						vestibulum mauris congue.
+					Welcome to NebTech, where our commitment to secure and convenient online shopping defines our mission. We understand the importance of trust and flexibility, and that's why we offer a pay-on-delivery service, putting you in control of your shopping experience. With a focus on security, we ensure you don't have to share your payment information online, providing peace of mind. Our process is simple: browse our diverse product selection, place your order, and pay only when your items arrive at your doorstep, allowing you to inspect and approve your purchase. We take pride in our reputation for reliability and trustworthiness, consistently delivering orders promptly and accurately. Our dedication to quality and transparency ensures that you receive high-quality products, and if you have any concerns, our responsive customer support team is ready to assist. We value your feedback and inquiries, and we're always here to help. Thank you for choosing NebTech as your trusted online shopping partner, and we look forward to serving you with the utmost satisfaction through our pay-on-delivery service.
 					</p>
 				</div>
 			</div>
